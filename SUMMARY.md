0366d1d229465eed6f3b1857e3d0ce3e8bb87c48
Fisher_C <guiku2007@qq.com>
2016-09-13 11:38:41 +0800

    add shell

--- SUMMARY.md
+++ SUMMARY.md
@@ -1,7 +1,7 @@
 # Summary
 
 * [首页](README.md)
-* [WeekNews](./weeknews/jijiang.md)
+* [周报](./weeknews/jijiang.md)
 	* [11th](./weeknews/11th/11th_original.md)
 * [日常知识整理](./tips/readme.md)
 	* [subl 快捷键](./tips/subl_shorcut.md)
---
+++ weeknews.sh
@@ -0,0 +1,39 @@
+
+# get img 
+
+img_type=".png"
+file_name=""
+
+if [ "$1" = "" ]
+then
+	echo Please enter your file_name:
+	read file_name 
+else
+	file_name=$1
+fi
+
+if [ -w $file_name ]
+then
+	echo "" >> $file_name
+	echo "" >> $file_name
+	n="1"
+	str_temp=""
+	for str in ` cat $file_name |grep [][] | awk -F'[][]' '{for(i=2;i<NF;i+=2) print $i}' `
+		do
+			if [ "$n" = "1" ]
+			then
+				str_temp="./img/"$str$img_type
+				n="2"
+			else
+				str_temp="["${str}"]:"$str_temp
+				echo $str_temp >> $file_name
+				str_temp=""
+				n="1"
+			fi
+	done
+	echo Done.
+else
+	echo "your file not exists or can't be writed,Please check your file."
+fi
+
+
